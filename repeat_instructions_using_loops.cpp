d0c9e6e1349d286e9600a0a5f174d27a8fd7494f
WizzOFF <maher_off@proton.me>
2025-03-27 05:41:29 +0100

    repeat_instructions_using_loops.cpp: do ... while

--- repeat_instructions_using_loops.cpp
+++ repeat_instructions_using_loops.cpp
@@ -1,4 +1,4 @@
-
+/*
 #include <iostream>
 
 using namespace std;
@@ -17,4 +17,23 @@ int main()
 
     return 0;
 }
+*/
 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nbEnfants(0);
+
+    do
+    {
+        cout << "Combien d'enfants avez-vous ?" << endl;
+        cin >> nbEnfants;
+    } while (nbEnfants < 0);
+
+    cout << "Merci d'avoir indique un nombre d'enfants correct. Vous en avez " << nbEnfants << endl;
+
+    return 0;
+}
