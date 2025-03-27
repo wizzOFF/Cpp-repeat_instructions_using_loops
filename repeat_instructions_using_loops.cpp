2085d81684d18369f0ce8f75598e7029e37a97ba
WizzOFF <maher_off@proton.me>
2025-03-27 05:31:59 +0100

    repeat_instructions_using_loops.cpp: while

---
+++ repeat_instructions_using_loops.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int nbEnfants(-1); // Nombre négatif pour pouvoir entrer dans la boucle
+
+    while (nbEnfants < 0)
+    {
+        cout << "Combien d'enfants avez-vous ?" << endl;
+        cin >> nbEnfants;
+    }
+
+    cout << "Merci d'avoir indique un nombre d'enfants correct. Vous en avez " << nbEnfants << endl;
+
+    return 0;
+}
+
